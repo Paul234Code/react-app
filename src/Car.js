ba98670ffb1e208dd55b28887f2ec0b429bb2096
Paul Faye <64893580+Paul234Code@users.noreply.github.com>
2021-12-18 19:21:26 -0500

    adding component

---
+++ src/Car.js
@@ -0,0 +1,6 @@
+// a component function
+function Car() {
+    return <h2>I am a blue car component</h2>;
+
+}
+export  default Car;
---
+++ src/Garage.js
@@ -0,0 +1,13 @@
+// a component of garage
+import Car from "./Car";
+
+function Garage() {
+    return (
+        <>
+            <h1>Who lives in my garage component</h1>
+            <Car/>
+        </>
+    );
+
+}
+export  default  Garage;
